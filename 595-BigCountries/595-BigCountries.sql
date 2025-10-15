2659791b854d65d1192c8572a84a08d6c2740ffa
AnkushRajabhoj <37548484+AnkushRajabhoj@users.noreply.github.com>
2025-10-15 05:58:16 +0530

    Create 595-BigCountries

---
+++ 595-BigCountries/595-BigCountries.sql
@@ -0,0 +1,7 @@
+-- Last updated: 15/10/2025, 05:58:15
+-- Write your PostgreSQL query statement below
+
+
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
