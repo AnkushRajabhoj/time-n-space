4254ffd4a1ae4f26057d66e3979905dbd5e2674c
AnkushRajabhoj <37548484+AnkushRajabhoj@users.noreply.github.com>
2025-10-15 05:57:29 +0530

    Create 1186-BuildingH2o

---
+++ 1186-BuildingH2o/1186-BuildingH2o.java
@@ -0,0 +1,23 @@
+// Last updated: 15/10/2025, 05:57:29
+class H2O {
+    Semaphore hydrogen, oxygen;
+
+    public H2O() {
+      hydrogen = new Semaphore(2);
+      oxygen = new Semaphore(0);
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line
+        hydrogen.acquire();
+        releaseHydrogen.run();
+        oxygen.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        oxygen.acquire(2);
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+		releaseOxygen.run();
+        hydrogen.release(2);
+    }
+}
