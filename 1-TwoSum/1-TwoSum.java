1e71b3c43792ee37b841a466c444cdca79e1e62c
AnkushRajabhoj <37548484+AnkushRajabhoj@users.noreply.github.com>
2025-10-15 06:03:18 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,34 @@
+// Last updated: 15/10/2025, 06:03:18
+// Approach 1: 
+//  num + num` = targer
+// for each num find the index of num` if present
+// Space = O(nums)
+// Time = O(nums)
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      // { num : index }
+      var indexMap = new HashMap<Integer, Integer>();
+
+      for(var i = 0; i < nums.length; i++) {
+        // num` = target - num
+        var numComplement = target - nums[i];
+
+        if (indexMap.containsKey(numComplement)) {
+          // found
+          return new int[]{indexMap.get(numComplement), i};
+        }
+
+        indexMap.put(nums[i], i);
+      }
+      // Not found
+      return new int[]{-1,-1};
+    }
+}
+
+// Approach 2:
+// If space is constraint
+// then sort -> two pointer 
+// low and high -> move towards centre -> find the target sum 
+// for any 3 and 3+ some we will use this method
