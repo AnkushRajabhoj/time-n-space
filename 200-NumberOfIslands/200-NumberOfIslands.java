404462b4d2f1aa41f81f33934ae824b6c38c07b2
AnkushRajabhoj <37548484+AnkushRajabhoj@users.noreply.github.com>
2025-10-15 06:02:23 +0530

    Create 200-NumberOfIslands

---
+++ 200-NumberOfIslands/200-NumberOfIslands.java
@@ -0,0 +1,27 @@
+// Last updated: 15/10/2025, 06:02:23
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count  = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] == '1') {
+                    count++;
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return count; 
+    }
+
+    void dfs(int i, int j, char[][] grid) {
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+        dfs(i+1, j, grid);
+        dfs(i-1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i, j-1, grid);
+    }
+}
